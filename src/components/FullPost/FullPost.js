74355ddbb2e2fafbab63aa04afd14d2274a7929c
jnemo44 <64214120+jnemo44@users.noreply.github.com>
2021-01-02 15:51:49 -0600

    Added the ability to select a specific post and scaled down the number of posts retrieved by a GET request

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,20 +1,47 @@
 import React, { Component } from 'react';
+import acios from 'axios';
 
 import './FullPost.css';
+import axios from 'axios';
 
 class FullPost extends Component {
-    render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
+    state = {
+        loadedPost: null
+    }
 
-        );
+
+    componentDidUpdate () {
+        if (this.props.id) {
+            //Only GET if no loaded post or loaded post and id's are different
+            if ( !this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+                axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+                .then(response => {
+                    this.setState({loadedPost: response.data});
+                });
+            }
+            
+        }
+    }
+    render () {
+        let post = <p style={{textAlign: 'center'}}>Select a Post</p>;
+        //Give time for the post to be retrieved
+        if (this.props.id) {
+            post = <p style={{textAlign: 'center'}}>Loading...</p>;
+        }
+        //Check to see if a post is selected
+        if (this.state.loadedPost) {
+            post = (
+                <div className="FullPost">
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.body}</p>
+                    <div className="Edit">
+                        <button className="Delete">Delete</button>
+                    </div>
+                </div>
+    
+            );
+        }
+        
         return post;
     }
 }
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,16 +7,48 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    state = {
+        posts : [],
+        selectedPostId: null
+    }
+
+    componentDidMount () {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                //Storing only 4 posts
+                const posts = response.data.slice(0, 4);
+                //Add data to post data from server
+                const updatedPosts = posts.map(post => {
+                    //Return JS Object and add author to post data
+                    return {
+                        ...post,
+                        author: 'Joey Poey'
+                    }
+                });
+                this.setState({posts: updatedPosts});
+            });
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    }
+
     render () {
+        const posts = this.state.posts.map(post => {
+            return <Post 
+                        key={post.id} 
+                        title={post.title}
+                        author={post.author}
+                        clicked={() => this.postSelectedHandler(post.id)}/>
+        });
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
