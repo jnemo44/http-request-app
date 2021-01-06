4b7e4a913ea65b2e78003c6de8431eb3d8504c8c
jnemo44 <64214120+jnemo44@users.noreply.github.com>
2021-01-05 19:56:04 -0600

    Tested the use of interceptors for global code execution

--- src/index.js
+++ src/index.js
@@ -3,6 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+//// Interceptors allow for global code execution
+// Used to add some common headers whenever a request is made
+axios.interceptors.request.use(request => {
+    console.log(request);
+    // Need to allow the request to be returned
+    return request;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+    console.log(response);
+    // Need to allow the request to be returned
+    return response;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
