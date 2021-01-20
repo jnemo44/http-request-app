41a9a63a2ac4e30df5b84f9389a2f7eef9c01fe1
jnemo44 <64214120+jnemo44@users.noreply.github.com>
2021-01-19 20:38:02 -0600

    Restructured app by moving components into containers in preperation for single page routing

---
+++ src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import axios from '../../../axios';
+
+import Post from '../../../components/Post/Post';
+import './Posts.css';
+
+class Posts extends Component {
+    state = {
+        posts : [],
+    }
+
+    componentDidMount () {
+        axios.get('/posts')
+            .then(response => {
+                //Storing only 4 posts
+                const posts = response.data.slice(0, 4);
+                //Add data to post data from server
+                const updatedPosts = posts.map(post => {
+                    //Return JS Object and add author to post data
+                    return {
+                        ...post,
+                        author: 'Joey Poey'
+                    }
+                });
+                this.setState({posts: updatedPosts});
+            })
+            .catch(error => {
+                console.log(error);
+                //this.setState({error: true});
+            });
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    }
+
+    render () {
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>
+        if(!this.state.error)
+        {
+            posts = this.state.posts.map(post => {
+                return <Post 
+                            key={post.id} 
+                            title={post.title}
+                            author={post.author}
+                            clicked={() => this.postSelectedHandler(post.id)}/>
+            });
+        }
+        return (
+            <section className="Posts">
+                {posts}
+            </section>
+        )
+    }
+}
+
+
+export default Posts;
+
+
+
