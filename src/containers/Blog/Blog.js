9f54c344f3f983c9c4fb363cd18b1858702c181a
jnemo44 <64214120+jnemo44@users.noreply.github.com>
2021-01-26 07:02:36 -0600

    Lazy loading of new post component added

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,12 +3,17 @@ import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
-import FullPost from "./FullPost/FullPost";
+//import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+
+//Lazy loading of new post component
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+})
 
 class Blog extends Component {
     state = {
-        auth: false
+        auth: true
     }
     render () {
         return (
@@ -40,7 +45,7 @@ class Blog extends Component {
                 {/*Switch allows only one route to load */}
                 <Switch>
                     {/*This is a guard */}
-                    {this.state.auth ? <Route path="/new-post" component={NewPost}/> : null}
+                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost}/> : null}
                     <Route path="/posts" component={Posts}/>
                     {/*A way to handle 404 error, it is a catch all*/}
                     <Route render={() => <h1>Not found</h1>}/>
